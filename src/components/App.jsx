09954fff97065b0b86012f84ba034f1080fd3cbc
Alinchyk <alina.mykhalchuk09@gmail.com>
2023-04-24 21:48:34 +0200

    add ContactForm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+import s from './App.module.css'
+
+export default class App extends Component {
+state = {
+  contacts: [],
+   filter: ''
+}
+
+  formSubmitHandler = data => {console.log(data);}
+  
+  render() {
+    return (
+    <div className={s.container}>
+    <h1 className={s.title}>Phonebook</h1>
+    <ContactForm onSubmit={this.formSubmitHandler}  />
+
+    <h2 className={s.title}>Contacts</h2>
+
+</div>)
+}
+}
